6be05ec817fbf50d12a7737260bbfd29ffdc74bd
Daniel Gnifle <Daniel Gnifle>
2016-09-28 23:21:56 +0200

    Small modifications to header menu animations

--- js/gnifcom3.js
+++ js/gnifcom3.js
@@ -1,11 +1,25 @@
 $( document ).ready( function() {
 
+	/**
+	 * Add Parallax effect to header
+	 */
 	$( '#header' ).parallax({
 		imageSrc:   'img/layout/header-background.jpg'
 	});
-	
+
+	/**
+	 * Add handler for menu toggle
+	 */
 	$( '.menu .toggle' ).on( 'click', function() {
 		$(this).toggleClass( 'active' );
+	});
+
+	/**
+	 * Make every submenu 3 px wider than initially to make room for
+	 * border-right transition effect.
+	 */
+	$( '.sub-container' ).each( function() {
+		$(this).css( 'width', ( $(this).width() + 3 ) + 'px' );
 	});
 
 });
